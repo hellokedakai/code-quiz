a3c7477d057715d24d8016f4dce8971c2e40e73d
Kedakai <nicoleteeth@ucla.edu>
2021-03-12 15:14:26 -0800

    quiz built and styles modified

--- quiz.js
+++ quiz.js
@@ -1,33 +1,30 @@
 var timer;
-var counter = 10;
-var displaytime = document.getElementById("time");
-var footer = document.getElementById("progress").innerHTML;
-var highscores = document.getElementById("highscores")
+var counter = 100;
+var countdownId = 0;
+var progress = document.getElementById("progress");
+var footer = document.getElementById("footer");
+var highscores = document.getElementById("highscores");
 
 
 //COUNTDOWN
-    function showtime() {
-        var replaced = displaytime.innerHTML.replace('Time: ','Time: ' + counter);
-        document.getElementById("time").appendChild(replaced);
-    }
 
-    function startcount() {
-        timer = window.setTimeout("countdown()",1000);
-        // console.log("countdown starts!");
-    }
+function start() {
+    countdownId = setInterval("countdown()", 1000);
+}
 
-    function countdown() {
+function countdown() {
+    if (counter > 0) {
         counter = counter - 1;
-        // console.log(counter);
-        if (counter === 0){
-            window.clearTimeout(timer);
-            timer= null;
-        }
-        else{
-            timer=window.setTimeout("countdown()",1000);
-            showtime();
-        }
+        document.getElementById("time").innerHTML = "Time: " + counter;
     }
+    else{
+        clearInterval(countdown);
+        document.getElementById("time").innerHTML = "GAME OVER";
+        showScores();
+    }
+}
+
+
 
 //SET UP QUIZ STUFF
     function Quiz(questions) {
@@ -43,13 +40,18 @@ var highscores = document.getElementById("highscores")
     
     Quiz.prototype.guess = function(answer) {
         if(this.getQuestionIndex().isCorrectAnswer(answer)) {
-            this.score++;
-            footer.innerHTML = "Correct!"
+            this.score = this.score + 10;
+            footer.style.visibility = "visible";
+            progress.innerHTML = "Correct!";
         } else {
-            footer.innerHTML = "Wrong!"
+            this.score = this.score - 10;
+            counter = counter - 10;
+            footer.style.visibility = "visible";
+            progress.innerHTML = "Wrong!"
         }
     
         this.questionIndex++;
+
     }
     
     Quiz.prototype.isEnded = function() {
@@ -70,7 +72,7 @@ var highscores = document.getElementById("highscores")
 //Replace start screen with quiz screen
     function quizstart() {
         //get startscreen H1
-        startH1 = document.getElementById("start1");
+        var startH1 = document.getElementById("startH1");
         var mainDiv = startH1.parentNode;
         //create quiz screen H1
         var quizH1 = document.createElement("H1");
@@ -78,44 +80,49 @@ var highscores = document.getElementById("highscores")
         //replace h1
         mainDiv.replaceChild(quizH1, startH1);
 
-        //remove p element from HTML
+        //remove p element
         var startP = document.getElementById("startP");
         startP.remove();
+        //remove start button
+        var startB = document.getElementById("startB");
+        startB.remove();
+        
 
         //get startscreeen button container
-        startBC = document.getElementById("start-button-container");
+        var startBC = document.getElementById("start-button-container");
         //create quiz button container
         var quizBC = document.createElement("DIV");
-        quizBC.setAttribute("class", "buttons");
-        //replace startB
+        quizBC.setAttribute("id", "buttons");
+        //replace startBC
         mainDiv.replaceChild(quizBC, startBC);
-
-        //remove start button
-        startB = document.getElementById("start-button");
-        startB.remove();
 
         //create buttons and append them to startBC
         for (var i=0; i < 4; i++) {
             var button = document.createElement("BUTTON");
             button.id = "btn" + i;
-            document.getElementByClassName("buttons").appendChild(button);
+            quizBC.appendChild(button);
 
             var span = document.createElement("SPAN");
-            span.id= "choice" + i;
-            document.getElementbyId(button.id).appendChild(span);
+            span.id = "choice" + i;
+            button.appendChild(span);
         }
 
+        
 
+
+
+        start();
         populate();
     };
  
 //Questions
     function populate() {
         if(quiz.isEnded()) {
+            
             showScores();
+            
         }
         else {
-            startcount();
             // show question
             var element = document.getElementById("question");
             element.innerHTML = quiz.getQuestionIndex().text;
@@ -128,7 +135,6 @@ var highscores = document.getElementById("highscores")
                 guess("btn" + i, choices[i]);
             }
     
-            // showProgress();
         }
     };
  
@@ -142,10 +148,21 @@ var highscores = document.getElementById("highscores")
  
  
 function showScores() {
-    var gameOverHTML = "<h1>Result</h1>";
-    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-    var element = document.getElementById("quiz");
+    //If the quiz score is a negative value, set score to zero.
+    if (quiz.score < 0) {
+        quiz.score = 0;
+    }
+    //Show results 
+    var gameOverHTML = "<h1>All done!</h1>";
+    gameOverHTML += "<h2 id='score'> Your final score is " + quiz.score + ". </h2>";
+    gameOverHTML += "Enter initials: "
+    var element = document.getElementById("challenge");
     element.innerHTML = gameOverHTML;
+
+    // var formHTML = "<input type='form'>";
+    // formHTML += "<input type='submit'>";
+    // var form = document.getElementById("form");
+    // form.innerHTML = formHTML;
 };
  
 // create questions here
@@ -154,12 +171,8 @@ var questions = [
     new Question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
     new Question("Which is not a JavaScript Framework?", ["Python Script", "JQuery","Django", "NodeJS"], "Django"),
     new Question("Which is used for Connect To Database?", ["PHP", "HTML", "JS", "All"], "PHP"),
-    new Question("Webdevtrick.com is about..", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All")
+    new Question("Frogs are usually good at...", ["Web Design", "Graphic Design", "SEO & Development", "All"], "All")
 ];
  
 // create quiz
 var quiz = new Quiz(questions);
- 
-// display quiz screen
-quizstart();
-populate();
