18a478460d0f1a9330514347a81e96e48a220511
Kedakai <nicoleteeth@ucla.edu>
2021-03-14 00:22:06 -0800

    add highscore to local storage & highscores screen

--- quiz.js
+++ quiz.js
@@ -1,36 +1,34 @@
 var timer;
-var counter = 5;
+var counter = 100;
 var countdownId = 0;
 var progress = document.getElementById("progress");
 var footer = document.getElementById("footer");
+
 var highscores = document.getElementById("highscores");
+highscores.addEventListener("click", finalScreen);
+
 var scoreSubmit = document.getElementById("score-submit");
 
+var startButton = document.getElementById("startB");
+startButton.addEventListener("click", quizstart);
 
 //COUNTDOWN
-
 function start() {
     countdownId = setInterval("countdown()", 1000);
 }
-
 function countdown() {
     if (counter > 0) {
         counter = counter - 1;
         document.getElementById("time").innerHTML = "Time: " + counter;
     }
     else {
-        // clearInterval(countdown);
-        document.getElementById("time").innerHTML = "GAME OVER";
+        clearInterval(countdownId);
         showScores();
     }
 }
-
-// function stopcount() {
-//     clearInterval(countdown);
-//     document.getElementById("time").innerHTML = "GAME OVER";
-// }
-
-
+function stopcount() {
+    clearInterval(countdownId);
+}
 
 //SET UP QUIZ STUFF
 function Quiz(questions) {
@@ -38,12 +36,10 @@ function Quiz(questions) {
     this.questions = questions;
     this.questionIndex = 0;
 }
-
 //add methods to Quiz by using prototype property 
 Quiz.prototype.getQuestionIndex = function() {
     return this.questions[this.questionIndex];
 }
-
 Quiz.prototype.guess = function(answer) {
     if(this.getQuestionIndex().isCorrectAnswer(answer)) {
         this.score = this.score + 10;
@@ -55,102 +51,96 @@ Quiz.prototype.guess = function(answer) {
         footer.style.visibility = "visible";
         progress.innerHTML = "Wrong!"
     }
-
     this.questionIndex++;
-
 }
-
 Quiz.prototype.isEnded = function() {
     return this.questionIndex === this.questions.length;
 }
-
 //This is an object constructor
 function Question(text, choices, answer) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
 }
-
 Question.prototype.isCorrectAnswer = function(choice) {
     return this.answer === choice;
 }
-
 //Replace start screen with quiz screen
-    function quizstart() {
-        //get startscreen H1
-        var startH1 = document.getElementById("startH1");
-        var mainDiv = startH1.parentNode;
-        //create quiz screen H1
-        var quizH1 = document.createElement("H1");
-        quizH1.setAttribute("id", "question");
-        //replace h1
-        mainDiv.replaceChild(quizH1, startH1);
+function quizstart() {
+    //get startscreen H1
+    var startH1 = document.getElementById("startH1");
+    var mainDiv = startH1.parentNode;
+    //create quiz screen H1
+    var quizH1 = document.createElement("H1");
+    quizH1.setAttribute("id", "question");
+    //replace h1
+    mainDiv.replaceChild(quizH1, startH1);
 
-        //remove p element
-        var startP = document.getElementById("startP");
-        startP.remove();
-        //remove start button
-        var startB = document.getElementById("startB");
-        startB.remove();
-        
+    //remove p element
+    var startP = document.getElementById("startP");
+    startP.remove();
+    //remove start button
+    var startB = document.getElementById("startB");
+    startB.remove();
+    
 
-        //get startscreeen button container
-        var startBC = document.getElementById("start-button-container");
-        //create quiz button container
-        var quizBC = document.createElement("DIV");
-        quizBC.setAttribute("id", "buttons");
-        //replace startBC
-        mainDiv.replaceChild(quizBC, startBC);
+    //get startscreeen button container
+    var startBC = document.getElementById("start-button-container");
+    //create quiz button container
+    var quizBC = document.createElement("DIV");
+    quizBC.setAttribute("id", "buttons");
+    //replace startBC
+    mainDiv.replaceChild(quizBC, startBC);
 
-        //create buttons and append them to startBC
-        for (var i=0; i < 4; i++) {
-            var button = document.createElement("BUTTON");
-            button.id = "btn" + i;
-            quizBC.appendChild(button);
+    //create buttons and append them to startBC
+    for (var i=0; i < 4; i++) {
+        var button = document.createElement("BUTTON");
+        button.id = "btn" + i;
+        quizBC.appendChild(button);
 
-            var span = document.createElement("SPAN");
-            span.id = "choice" + i;
-            button.appendChild(span);
-        }
+        var span = document.createElement("SPAN");
+        span.id = "choice" + i;
+        button.appendChild(span);
+    }
 
-        
+    
 
 
 
-        start();
-        populate();
-    };
+    start();
+    populate();
+};
  
 //Questions
-    function populate() {
-        if(quiz.isEnded()) {
-            stopcount();
-            showScores();
-            
+function populate() {
+    if(quiz.isEnded()) {
+        stopcount();
+        showScores();
+        
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
         }
-        else {
-            // show question
-            var element = document.getElementById("question");
-            element.innerHTML = quiz.getQuestionIndex().text;
-    
-            // show options
-            var choices = quiz.getQuestionIndex().choices;
-            for(var i = 0; i < choices.length; i++) {
-                var element = document.getElementById("choice" + i);
-                element.innerHTML = choices[i];
-                guess("btn" + i, choices[i]);
-            }
-    
-        }
-    };
+
+    }
+};
  
-    function guess(id, guess) {
-        var button = document.getElementById(id);
-        button.onclick = function() {
-            quiz.guess(guess);
-            populate();
-        }
-    };
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
  
  
 function showScores() {
@@ -167,38 +157,41 @@ function showScores() {
     quizH1.appendChild(results);
     document.getElementById("buttons").remove();
     scoreSubmit.style.visibility = "visible";
+    var submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", saveScore);
 };
 
 function saveScore() {
-    var initials = document.getElementById("form").innerHTML;
-    localStorage.setItem(initials, quiz.score);
+    var initials = document.getElementById("initials").value;
+    const highScoreData = {
+        name: initials,
+        score: quiz.score,
+    }
+    window.localStorage.setItem('scoredata', JSON.stringify(highScoreData));
+    // localStorage.setItem(initials, quiz.score);
     finalScreen();
-
 }
+
+
 
 function finalScreen() {
-    var quizH1 = document.getElementById("question");
-    quizH1.innerHTML = "High scores"
-    
+    var mainDiv = document.getElementById("challenge");
+    mainDiv.innerHTML = '';
+    // mainDiv.replaceChild(scoreBoard, scoreSubmit);
+
+    var highScoreH1 = document.createElement("H1");
+    highScoreH1.innerHTML = "High scores";
+    mainDiv.appendChild(highScoreH1);
+
     var scoreBoard = document.createElement("P");
-    scoreBoard.innerHTML = localStorage.getItem("initials");
-    
-    var main = document.getElementById("challenge");
-    main.appendChild(scoreBoard);
+    scoreBoard.innerHTML = localStorage.getItem("scoredata");
+    mainDiv.appendChild(scoreBoard);
 
-    // var results = document.getElementById("results");
-    // results.style.vsibility = "hidden";
-
-    scoreSubmit.style.visibility = "hidden";
-
-    footer.style.visibility = "hidden";
-
-
-
-    
-
-    
+    var header = document.getElementById("top-row");
+    header.style.visibility = "hidden";
 }
+
+
  
 // create questions here
 var questions = [
